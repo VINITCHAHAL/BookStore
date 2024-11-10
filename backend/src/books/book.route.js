12a4e8da443599de07ae1f07d905f1ef0081b46f
VINITCHAHAL <vinitchahal9797@gmail.com>
2024-11-10 14:33:19 +0530

    Auth and Verification added

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const Book = require('./book.model');
 const { postABook, getAllBooks, getSingleBook, UpdateBook, deleteABook } = require('./book.controller');
-// const verifyAdminToken = require('../middleware/verifyAdminToken');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
 const router =  express.Router();
 // frontend => backend server => controller => book schema  => database => send to server => back to the frontend
-router.post("/create-book", postABook)
+router.post("/create-book", verifyAdminToken , postABook)
 router.get("/", getAllBooks);
 router.get("/:id", getSingleBook);
-router.put("/edit/:id", UpdateBook);
-router.delete("/:id", deleteABook)
+router.put("/edit/:id", verifyAdminToken , UpdateBook);
+router.delete("/:id", verifyAdminToken , deleteABook)
 module.exports = router;
